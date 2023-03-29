5b274edda1122946f35466374ebf430ea7d67e67
leomaurodesenv <leo.mauro.desenv@gmail.com>
2023-03-29 19:29:30 -0300

    add: random forest, logistic regression, kfold validation

--- kedro-sklearn/src/kedro_sklearn/pipelines/training/nodes.py
+++ kedro-sklearn/src/kedro_sklearn/pipelines/training/nodes.py
@@ -5,6 +5,7 @@ generated using Kedro 0.18.7
 
 import time
 import logging
+import numpy as np
 import pandas as pd
 from typing import Dict, List, Tuple, Union
 
@@ -12,53 +13,137 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_validate
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def _run_grid_search(model, params, train_X: any, train_y: any):
-    # Testing
-    search = GridSearchCV(model, params)
+def _run_grid_search(
+    model: any, params: Dict[any, any], train_X: any, train_y: any, k: int = 5
+) -> Tuple:
+    """GridSearch Hyperparameter Tuning
+
+    Args:
+        model: Model
+        params: Hyperparameters
+        train_X: Train data
+        train_y: Train label
+        k: Number of validations
+    Returns:
+        metrics: Avg results
+    """
+    search = GridSearchCV(
+        model, params, refit="f1_macro", cv=k, scoring=["accuracy", "f1_macro"]
+    )
     search.fit(train_X, train_y)
 
+    metrics = {
+        "accuracy": np.mean(search.cv_results_["mean_test_accuracy"]),
+        "f1": np.mean(search.cv_results_["mean_test_f1_macro"]),
+    }
+
+    # Logging
     LOGGER.info(search.best_estimator_)
-    LOGGER.info(search.best_estimator_.get_params()["classifier"])
+    LOGGER.info(search.best_params_)
+
+    return search.best_estimator_, metrics
 
 
-def train_logistic_regression(train_X: any, train_y: any) -> None:
-    # params = {
-    #     "C": [10**-2, 10**-1, 10**0],
-    #     "kernel": ["linear", "rbf", "sigmoid"],
-    #     "class_weight": [None, {0:1,1:3}, {0:1,1:5}],
-    # }
+def _run_k_fold(model: any, train_X: any, train_y: any, k: int = 5) -> Dict:
+    """k-Fold Cross Validation
 
-    # _run_grid_search(SVC(), params, train_X, train_y)
-    pass
+    Args:
+        model: Model
+        train_X: Train data
+        train_y: Train label
+        k: Number of validations
+    Returns:
+        metrics: Avg results
+    """
+    cv_results = cross_validate(
+        model, train_X, train_y, cv=k, scoring=["accuracy", "f1_macro"]
+    )
+    metrics = {
+        "accuracy": np.mean(cv_results["test_accuracy"]),
+        "f1": np.mean(cv_results["test_f1_macro"]),
+    }
+
+    return metrics
 
 
-def train_random_forest(train_X: any, train_y: any) -> None:
-    pass
+def train_logistic_regression(train_X: any, train_y: any) -> LogisticRegression:
+    """Train Logistic Regression
+
+    Args:
+        train_X: Train data
+        train_y: Train label
+    Returns:
+        model: sklearn.linear_model.LogisticRegression
+    """
+    runtime = time.time()
+    params = {
+        "max_iter": [5_000],
+        "penalty": [None, "l2"],
+        "class_weight": [None, {0: 1, 1: 3}, {0: 1, 1: 5}],
+    }
+    model, metrics = _run_grid_search(
+        model=LogisticRegression(), params=params, train_X=train_X, train_y=train_y
+    )
+    runtime = time.time() - runtime
+
+    # Logging
+    LOGGER.info("## Random Forest Training")
+    LOGGER.info("Avg Accuracy %.4f seconds" % metrics["accuracy"])
+    LOGGER.info("Avg F1-score %.4f seconds" % metrics["f1"])
+    LOGGER.info("Runtime elapsed %.4f seconds" % runtime)
+
+    return model
+
+
+def train_random_forest(train_X: any, train_y: any) -> RandomForestClassifier:
+    """Train Random Forest
+
+    Args:
+        train_X: Train data
+        train_y: Train label
+    Returns:
+        model: sklearn.ensemble.RandomForestClassifier
+    """
+    runtime = time.time()
+    model = RandomForestClassifier(n_estimators=100, max_depth=4, random_state=0)
+    metrics = _run_k_fold(model=model, train_X=train_X, train_y=train_y)
+    model.fit(train_X, train_y)
+    runtime = time.time() - runtime
+
+    # Logging
+    LOGGER.info("## Random Forest Training")
+    LOGGER.info("Avg Accuracy %.4f seconds" % metrics["accuracy"])
+    LOGGER.info("Avg F1-score %.4f seconds" % metrics["f1"])
+    LOGGER.info("Runtime elapsed %.4f seconds" % runtime)
+
+    return model
 
 
 def train_svc(train_X: any, train_y: any) -> SVC:
     """Train Support Vector Classifier (SVC)
 
     Args:
-        train_X: train data
-        train_y: train label
+        train_X: Train data
+        train_y: Train label
     Returns:
         model: sklearn.svm.SVC
     """
     runtime = time.time()
-    model = SVC(kernel="rbf", gamma='auto')
+    model = SVC(kernel="rbf", gamma="auto", random_state=0)
+    metrics = _run_k_fold(model=model, train_X=train_X, train_y=train_y)
     model.fit(train_X, train_y)
     runtime = time.time() - runtime
 
     # Logging
     LOGGER.info("## SVC Training")
+    LOGGER.info("Avg Accuracy %.4f seconds" % metrics["accuracy"])
+    LOGGER.info("Avg F1-score %.4f seconds" % metrics["f1"])
     LOGGER.info("Runtime elapsed %.4f seconds" % runtime)
 
     return model
--- kedro-sklearn/src/kedro_sklearn/pipelines/training/pipeline.py
+++ kedro-sklearn/src/kedro_sklearn/pipelines/training/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import train_svc
+from .nodes import train_svc, train_random_forest, train_logistic_regression
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -12,9 +12,30 @@ def create_pipeline(**kwargs) -> Pipeline:
         [
             node(
                 func=train_svc,
-                inputs=["train_X", "train_y"],
+                inputs=dict(
+                    train_X="train_X",
+                    train_y="train_y",
+                ),
                 outputs="model_svc",
                 name="train_svc_node",
-            )
+            ),
+            node(
+                func=train_random_forest,
+                inputs=dict(
+                    train_X="train_X",
+                    train_y="train_y",
+                ),
+                outputs="model_random_forest",
+                name="train_random_forest_node",
+            ),
+            node(
+                func=train_logistic_regression,
+                inputs=dict(
+                    train_X="train_X",
+                    train_y="train_y",
+                ),
+                outputs="model_logistic_regression",
+                name="train_logistic_regression_node",
+            ),
         ]
     )
