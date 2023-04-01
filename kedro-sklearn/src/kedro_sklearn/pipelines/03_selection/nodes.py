cbbe2823f363e5cbccd3e741dd93c2326817c864
leomaurodesenv <leo.mauro.desenv@gmail.com>
2023-03-31 22:00:55 -0300

    update: model selection, add: submission generation

--- kedro-sklearn/src/kedro_sklearn/pipelines/03_selection/nodes.py
+++ kedro-sklearn/src/kedro_sklearn/pipelines/03_selection/nodes.py
@@ -2,3 +2,58 @@
 This is a boilerplate pipeline 'selection'
 generated using Kedro 0.18.7
 """
+
+import logging
+import pandas as pd
+from typing import Dict, List, Tuple, Union
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def model_selection(metric_name: str, **kargs) -> any:
+    """Model selection
+
+    Args:
+        model_{index}: Model
+        metric_{index}: Model metrics
+        metric_name: Principal metric
+    Returns:
+        best_model: Best model according to the metric
+    """
+    idx = 0
+    best_model = kargs["model_0"]
+    best_score = kargs["metric_0"][metric_name]
+
+    while True:
+        if not f"model_{idx}" in kargs:
+            break
+        if kargs[f"metric_{idx}"][metric_name] > best_score:
+            best_model = kargs[f"model_{idx}"]
+            best_score = kargs[f"metric_{idx}"][metric_name]
+        idx = int(idx + 1)
+
+    # Logging
+    LOGGER.info("## Best model")
+    LOGGER.info(best_model)
+    LOGGER.info(f"{metric_name}: {best_score:.4f}")
+
+    return best_model
+
+
+def model_prediction(model: any, X_test: any, test_set: pd.DataFrame):
+    """Model prediction
+
+    Args:
+        model: Best model
+        X_test: Input test data
+        test_set: Test dataset
+    Returns:
+        submission: Submission file
+    """
+    target  = model.predict(X_test)
+    id_test = test_set["id"].to_list()
+    submission = {'id':id_test, 'target':target}
+    df_submission = pd.DataFrame(submission)
+
+    return df_submission
--- kedro-sklearn/src/kedro_sklearn/pipelines/03_selection/pipeline.py
+++ kedro-sklearn/src/kedro_sklearn/pipelines/03_selection/pipeline.py
@@ -4,7 +4,36 @@ generated using Kedro 0.18.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from .nodes import model_selection, model_prediction
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=model_selection,
+                inputs=dict(
+                    model_0="model_svc",
+                    model_1="model_random_forest",
+                    model_2="model_logistic_regression",
+                    metric_0="metrics_svc",
+                    metric_1="metrics_random_forest",
+                    metric_2="metrics_logistic_regression",
+                    metric_name=f"params:{metric}.metric",
+                ),
+                outputs=f"{metric}.selected_model",
+                name=f"{metric}.model_selection_node",
+            )
+            for metric in ["f1", "accuracy"]
+        ] + [
+            node(
+                func=model_prediction,
+                inputs=dict(
+                    model="f1.selected_model",
+                    X_test="test_X",
+                    test_set="test",
+                ),
+                outputs="submission",
+                name="model_prediction_node",
+            )
+        ]
+    )
